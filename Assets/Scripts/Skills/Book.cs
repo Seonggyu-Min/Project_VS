9e253bb8658799cc55abb824db75f191d5811021
Seonggyu-Min <minsunggu705@gmail.com>
2025-05-30 00:32:27 +0900

    test: 책 스킬 구현 테스트
    - 플레이어 주위를 돌고 projectileNumber 만큼 생성할 수 있는지 테스트하기 위한 로직 생성

---
+++ Assets/Scripts/Skills/Book.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class Book : BaseSkill
+//{
+    //[SerializeField] private BaseSkill _bookPiecePrefab;
+
+    //private List<BaseSkill> _spawnedPieces = new();
+
+    //public override void InitSkillPosition()
+    //{
+    //    StartCoroutine(SpawnAndRotateRoutine());
+    //}
+
+    //private IEnumerator SpawnAndRotateRoutine()
+    //{
+    //    float angleStep = 360f / _projectileNumber;
+
+    //    for (int i = 0; i < _projectileNumber; i++)
+    //    {
+    //        float angle = angleStep * i;
+
+    //        BaseSkill piece = GameManager.Instance.SkillPoolManager.GetSkillInstance(_bookPiecePrefab);
+    //        piece.SetPlayerReferences(_playerTransform, _playerMove);
+
+    //        BookPiece book = piece as BookPiece;
+    //        book.Init(_playerTransform, angle, _size, _projectileSpeed);
+
+    //        _spawnedPieces.Add(book);
+    //    }
+
+    //    yield return new WaitForSeconds(_duration);
+
+    //    foreach (var piece in _spawnedPieces)
+    //    {
+    //        GameManager.Instance.SkillPoolManager.ReturnSkillInstance(piece);
+    //    }
+
+    //    _spawnedPieces.Clear();
+    //    GameManager.Instance.SkillPoolManager.ReturnSkillInstance(this); // BookSkill 자체도 반환
+    //}
+//}
---
+++ Assets/Scripts/Skills/BookPiece.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class BookPiece : BaseSkill
+//{
+    //private float _angle;
+    //private float _radius;
+    //private float _rotationSpeed;
+    //private Transform _center;
+
+    //public void Init(Transform center, float angle, float radius, float speed)
+    //{
+    //    _center = center;
+    //    _angle = angle;
+    //    _radius = radius;
+    //    _rotationSpeed = speed;
+    //    gameObject.SetActive(true);
+    //}
+
+    //private void Update()
+    //{
+    //    _angle += _rotationSpeed * Time.deltaTime;
+    //    float rad = _angle * Mathf.Deg2Rad;
+
+    //    Vector3 offset = new Vector3(Mathf.Cos(rad), Mathf.Sin(rad)) * _radius;
+    //    transform.position = _center.position + offset;
+    //}
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (((1 << collision.gameObject.layer) & _monsterLayermask) != 0)
+    //    {
+    //        ReferenceProvider provider = ReferenceRegistry.GetProvider(collision.gameObject);
+
+    //        if (provider == null)
+    //        {
+    //            Debug.LogWarning($"ReferenceProvider가 {collision.gameObject.name}에 없음");
+    //            return;
+    //        }
+
+    //        IDamageable damageable = provider.GetAs<IDamageable>();
+
+    //        if (damageable != null)
+    //        {
+    //            damageable.TakeDamage(_damage);
+    //            //Debug.Log($"{_damage}만큼 데미지 입힘");
+    //        }
+    //        else
+    //        {
+    //            Debug.LogWarning($"IDamageable이 {collision.gameObject.name}에 없음");
+    //        }
+
+    //    }
+    //}
+
+    //public override void InitSkillPosition()
+    //{
+        
+    //}
+//}

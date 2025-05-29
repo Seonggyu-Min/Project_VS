8daad8cd24e49b92ac1ce2a0906cb586cf674109
Seonggyu-Min <minsunggu705@gmail.com>
2025-05-30 00:39:30 +0900

    feat: 몬스터 스탯용 ScriptableObject 구현
    - 몬스터의 밸런스 및 관리 편의를 위해 스탯을 필드로 가진 ScriptableObject 생성

---
+++ Assets/Scripts/ScriptableObjects/Monsters/MonstersSO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MonstersSO", menuName = "ScriptableObjects/MonstersSO", order = 0)]
+public class MonstersSO : ScriptableObject
+{
+    public string MonsterName;
+    public int Damage;
+    public float Speed;
+    public int MaxHealth;
+    public int DropExp;
+}

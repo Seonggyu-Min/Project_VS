adecab5b70600dec12d9743cfdd92cb40940fc91
Seonggyu-Min <minsunggu705@gmail.com>
2025-05-30 00:34:38 +0900

    feat: WaterBolt 스킬 구현
    - 플레이어가 바라보고 있는 방향에서 약간의 랜덤 각도를 추가하여 생성하는 로직 구현

---
+++ Assets/Scripts/Skills/WaterBolt.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBolt : BaseSkill
+{
+    [SerializeField] private CapsuleCollider2D _collider;
+
+
+    protected override void Update()
+    {
+        base.Update();
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector3.right * _projectileSpeed * Time.deltaTime);
+    }
+
+    public override void InitSkillPosition()
+    {
+        float angle = Mathf.Atan2(_playerMove.PlayerLookVector.y, _playerMove.PlayerLookVector.x) * Mathf.Rad2Deg;
+
+        float offset = Random.Range(-10f, 10f);
+
+        transform.rotation = Quaternion.Euler(0, 0, angle + offset);
+        transform.position = _playerTransform.position;
+    }
+}

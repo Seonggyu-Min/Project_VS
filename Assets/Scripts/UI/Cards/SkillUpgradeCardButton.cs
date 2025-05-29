30d4eee19d5600191bada1f2622bafa05a0a7599
Seonggyu-Min <minsunggu705@gmail.com>
2025-05-30 00:28:30 +0900

    feat: 스킬 업그레이드 시스템 구현
    - 해당하는 스킬의 스탯을 상승시킴

---
+++ Assets/Scripts/UI/Cards/SkillUpgradeCardButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+public class SkillUpgradeCardButton : MonoBehaviour
+{
+    [SerializeField] private UpgradeCardsSO _upgradeCardsSO;
+
+    // 텍스트
+    [SerializeField] private TMP_Text _upperText;
+    [SerializeField] private TMP_Text _lowerText;
+
+    private StringBuilder _upSb = new StringBuilder();
+    private StringBuilder _lowSb = new StringBuilder();
+
+    private void OnEnable()
+    {
+        _upSb.Clear();
+        _upSb.AppendLine(_upgradeCardsSO.SkillName);
+        _upperText.text = _upSb.ToString();
+
+        _lowSb.Clear();
+        _lowSb.AppendLine("Upgrade Skill");
+        _lowerText.text = _lowSb.ToString();
+    }
+
+    public void OnClickCard()
+    {
+        _upgradeCardsSO?.ApplyUpgradeCard();
+
+        CardDrawManager.Instance.EndEffectOfCardSelection();
+    }
+}

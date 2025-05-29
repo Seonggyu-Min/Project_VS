2d7c9b2c079646152131518689640482221b2a99
Seonggyu-Min <minsunggu705@gmail.com>
2025-05-30 00:41:16 +0900

    feat: 업그레이드 카드 스탯용 ScriptableObject 구현
    - 업그레이드 스탯의 밸런스 및 관리 편의를 위해 스탯을 필드로 가진 ScriptableObject 생성
    - 카드의 이름을 ICard 인터페이스로 묶어 스킬 업그레이드용 및 플레이어 스탯 업그레이드용 메서드 분리 호출
    - 추후 역할 분리 고려

---
+++ Assets/Scripts/ScriptableObjects/Cards/Upgrades/UpgradeCardsSO.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UpgradeCardsSO", menuName = "ScriptableObjects/UpgradeCardsSO", order = 0)]
+public class UpgradeCardsSO : ScriptableObject, ICard
+{
+    public CardUpgradeType UpgradeType;
+    public string SkillName;
+
+    // 누적합 방식으로 적용되는 데이터, 0.n 으로 범위 조절
+    [Header("Skill Upgrade Data")]
+    public float DamageMultiplier = 0f;
+    public float CooldownMultiplier = 0f;
+    public float ProjectileSpeedMultiplier = 0f;
+    public float ProjectileNumberMultiplier = 0f;
+    public float SizeMultiplier = 0f;
+    public float DurationMultiplier = 0f;
+    public float KnockbackForceMultiplier = 0f;
+
+    [Header("Player Stat Upgrade Data")]
+    public float HealthMultiplier = 0f;
+    public float HealthRegenMultiplier = 0f;
+    public float ArmorMultiplier = 0f;
+    public float MoveSpeedMultiplier = 0f;
+    public float GracePeriodMultiplier = 0f;
+    public float ExpMultiplier = 0f;
+    public float GoldMultiplier = 0f;
+    public float MagnetRangeMultiplier = 0f;
+    public float LuckMultiplier = 0f;
+    public int ReviveMultiplier = 0;
+
+
+    string ICard.SkillName => SkillName;
+
+    public void ApplyUpgradeCard()
+    {
+        switch (UpgradeType)
+        {
+            case CardUpgradeType.SkillUpgrade:
+                SkillManager.Instance.ApplyCardUpgrade(this);
+                break;
+            case CardUpgradeType.StatUpgrade:
+                PlayerStatManager.Instance.ApplyCardUpgrade(this);
+                break;
+            default:
+                Debug.LogWarning("Unknown Upgrade Type");
+                break;
+        }
+    }
+}
+
+public enum CardUpgradeType
+{
+    SkillUpgrade = 0,
+    StatUpgrade
+}

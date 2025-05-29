62f2b46ad518c3f5bc04485692d2752cb1955324
Seonggyu-Min <minsunggu705@gmail.com>
2025-05-30 00:27:33 +0900

    feat: 스킬 최초 획득 시스템 구현
    - UI에 자신의 정보를 텍스트로 출력하는 기능 구현
    - 버튼 UI가 눌렸을 때 해당 스킬을 추가하는 시스템 구현
    - 최초 획득 시에만 필요한 필요한 카드이기에 UI 카드 리스트에서 자기 자신 삭제 및 관련 스킬 업그레이드 카드 등록

---
+++ Assets/Scripts/UI/Cards/GetSkillCardButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+public class GetSkillCardButton : MonoBehaviour
+{
+    [SerializeField] private GetSkillCardsSO _getSkillcardSO;
+    [SerializeField] private GameObject _relatedCardButton;
+    // 텍스트
+    [SerializeField] private TMP_Text _upperText;
+    [SerializeField] private TMP_Text _lowerText;
+
+    private StringBuilder _upSb = new StringBuilder();
+    private StringBuilder _lowSb = new StringBuilder();
+
+    private void OnEnable()
+    {
+        _upSb.Clear();
+        _upSb.AppendLine(_getSkillcardSO.SkillName);
+        _upperText.text = _upSb.ToString();
+
+        _lowSb.Clear();
+        _lowSb.AppendLine("Get New Skill");
+        _lowerText.text = _lowSb.ToString();
+    }
+
+    public void OnClickCard()
+    {
+        _getSkillcardSO?.ApplyGetSkillCard();
+
+        // 자기 자신 삭제 및 업그레이드 카드 등록
+        CardDrawManager.Instance.RegisterAndRemoveCard(_relatedCardButton, gameObject);
+
+        CardDrawManager.Instance.EndEffectOfCardSelection();
+
+        //Destroy(gameObject);
+    }
+}

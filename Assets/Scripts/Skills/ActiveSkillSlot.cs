905e926f0fff2f2f070bbc4ff484af2c2eb83e74
Seonggyu-Min <minsunggu705@gmail.com>
2025-05-30 00:37:50 +0900

    feat: 플레이어가 소지한 스킬 슬롯 관리 시스템 구현
    - 해당 슬롯은 new로 생성되어야 하기에 MonoBehaviour를 상속받지 않은 일반 C# 클래스로 생성함
    - 각 스킬마다 업그레이드 된 수치가 달라야하기에 해당 스탯을 필드로 저장
    - 코루틴을 통해 스킬을 쿨다운에 맞게 생성

---
+++ Assets/Scripts/Skills/ActiveSkillSlot.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.Utilities;
+
+public class ActiveSkillSlot
+{
+    public SkillsSO Data { get; private set; }
+
+    private float _damageMultiplier = 1f;
+    private float _cooldownMultiplier = 1f;
+    private float _projectileNumberMultiplier = 1f;
+    private float _projectileSpeedMultiplier = 1f;
+    private float _sizeMultiplier = 1f;
+    private float _durationMultiplier = 1f;
+    private float _knockbackForceMultiplier = 1f;
+
+
+    public float Cooldown => Data.Cooldown;
+    private float _cooldownTimer;
+    public ObservableProperty<bool> IsReady { get; private set; } = new();
+
+    private Coroutine _cooldownCoroutine;
+
+    private MonoBehaviour _context;
+
+
+    public ActiveSkillSlot(SkillsSO data, MonoBehaviour context)
+    {
+        Data = data;
+        _context = context;
+
+        IsReady.Value = true; // 초기 상태는 항상 사용 가능
+    }
+
+    public void TryUseSkill(Transform playerTransform, PlayerMove playerMove)
+    {
+        if (!IsReady.Value) return;
+
+        BaseSkill skill = SkillPoolManager.Instance.GetSkillInstance(Data.Prefab);
+        skill.SetPlayerReferences(playerTransform, playerMove);
+        skill.InitSkillPosition();
+
+        skill.UpgradeSkill(
+            damage: _damageMultiplier,
+            cd: _cooldownMultiplier,
+            projectileNumber: _projectileNumberMultiplier,
+            projectileSpeed: _projectileSpeedMultiplier,
+            size: _sizeMultiplier,
+            duration: _durationMultiplier,
+            knockbackForce: _knockbackForceMultiplier
+            );
+
+        _cooldownCoroutine = _context.StartCoroutine(CooldownRoutine());
+    }
+
+    private IEnumerator CooldownRoutine()
+    {
+        IsReady.Value = false;
+        _cooldownTimer = 0f;
+
+        while (_cooldownTimer < Cooldown)
+        {
+            _cooldownTimer += Time.deltaTime;
+            yield return null;
+        }
+
+        IsReady.Value = true;
+    }
+
+
+    public void ApplyUpgradeFromCard(UpgradeCardsSO card)
+    {
+        _damageMultiplier += card.DamageMultiplier;
+        _cooldownMultiplier += card.CooldownMultiplier;
+        _projectileNumberMultiplier += card.ProjectileNumberMultiplier;
+        _projectileSpeedMultiplier += card.ProjectileSpeedMultiplier;
+        _sizeMultiplier += card.SizeMultiplier;
+        _durationMultiplier += card.DurationMultiplier;
+        _knockbackForceMultiplier += card.KnockbackForceMultiplier;
+    }
+
+
+    // TODO: 쿨다운 업그레이드가 되면 이벤트 발생되는 구조, 해당 이벤트에 이 메서드 구독처리 할 것
+    //public void UpdateCooldownValue(float newValue)
+    //{
+    //    Data.Cooldown = newValue;
+    //}
+}

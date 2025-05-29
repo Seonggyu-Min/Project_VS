550544b7596afac3a2589c420adda0ec0495e8ec
Seonggyu-Min <minsunggu705@gmail.com>
2025-05-30 00:43:41 +0900

    feat: 스킬 획득용 ScriptableObject 구현
    - SkillName과 대응하는 SkillSO를 등록하여 카드 획득 시 스킬 추가 처리
    - SkillManager와 연동하여 카드 선택 → 스킬 획득 흐름 구축

---
+++ Assets/Scripts/ScriptableObjects/Cards/GetSkills/GetSkillCardsSO.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "GetSkillCardsSO", menuName = "ScriptableObjects/GetSkillCardsSO", order = 1)]
+public class GetSkillCardsSO : ScriptableObject, ICard
+{
+    public string SkillName;
+    public SkillsSO SkillsSO;
+
+    string ICard.SkillName => SkillName;
+
+    public void ApplyGetSkillCard()
+    {
+        SkillManager.Instance.AddSkill(SkillsSO);
+    }
+}

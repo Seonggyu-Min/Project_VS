96ca5613b27ec0536011ae5022308fc95268a33d
Seonggyu-Min <minsunggu705@gmail.com>
2025-05-30 00:38:49 +0900

    feat: 스킬 스탯용 ScriptableObject 구현
    - 스킬의 밸런스 및 관리 편의를 위해 스탯을 필드로 가진 ScriptableObject 생성

---
+++ Assets/Scripts/ScriptableObjects/Skills/SkillsSO.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SkillsSO", menuName = "ScriptableObjects/SkillsSO", order = 0)]
+public class SkillsSO : ScriptableObject
+{
+    public string SkillName;
+
+    public int Damage;
+    public float Cooldown;
+    public int ProjectileNumber;
+    public float ProjectileSpeed;
+    public float Size;
+    public float Duration;
+    public float KnockbackForce;
+
+    public BaseSkill Prefab;
+}

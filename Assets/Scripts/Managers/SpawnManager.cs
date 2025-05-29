327b69d85158a789bdce084daa6e27faf34dda24
Seonggyu-Min <minsunggu705@gmail.com>
2025-05-30 00:50:44 +0900

    feat: 몬스터 스폰 시스템 구현
    - 몬스터 프리팹을 List로 가지고 이를 통해 플레이어 주변에서 몬스터 생성하는 로직 구현
    - 추후 플레이어 주변이면서 동시에 카메라 밖에서 생성되도록 변경 예정
    - 추후 시간 별로 몬스터 생성 종류 변경 로직 추가 예정

---
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private Dictionary<BaseMonster, ObjectPool<BaseMonster>> _pools = new ();
+
+    [SerializeField] private List<BaseMonster> monsterPrefabs;
+
+    [SerializeField] private float _spawnTimer;
+    [SerializeField] private float _spawnTimerMax = 2f;
+
+    [SerializeField] private Transform _target;
+
+
+    public static SpawnManager Instance { get; private set; }
+
+
+    private void Awake()
+    {
+        Init();
+
+        // 싱글톤 처럼 사용하기 위함
+        Instance = this;
+    }
+
+
+
+    private void Update()
+    {
+        _spawnTimer += Time.deltaTime;
+
+        if (_spawnTimer >= _spawnTimerMax)
+        {
+            _spawnTimer = 0f;
+
+            Spawn(monsterPrefabs[0], transform.position, _target);
+        }
+    }
+
+
+    private void OnDestroy()
+    {
+        Instance = null;
+    }
+
+
+
+    private void Init()
+    {
+        foreach (BaseMonster prefab in monsterPrefabs)
+        {
+            _pools[prefab] = new ObjectPool<BaseMonster>(transform, prefab, 20);
+        }
+    }
+
+    private BaseMonster Spawn(BaseMonster prefab, Vector2 pos, Transform target)
+    {
+        BaseMonster monster = _pools[prefab].PopPool();
+        monster.transform.position = pos;
+        monster.InitBaseMonster(target);
+        return monster;
+    }
+}

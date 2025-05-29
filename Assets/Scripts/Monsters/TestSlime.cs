2ba8c07b15c244c41d4045ad9ff9ec02938dbd15
Seonggyu-Min <minsunggu705@gmail.com>
2025-05-30 00:21:48 +0900

    test: 플레이어 추적 확인용 적 구현
    테스트용 플레이어를 추적하는 적 구현

---
+++ Assets/Scripts/Monsters/TestSlime.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class TestSlime : BaseMonster
+//{
+//    public override int Health { get; set; }
+//    public override int MaxHealth { get; } = 100;
+//    public override float MoveSpeed { get; set; } = 1f;
+
+
+//    public override void InitChildMonster()
+//    {
+//        MoveSpeed = 1f;
+//    }
+//}

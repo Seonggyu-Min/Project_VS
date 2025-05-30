96f69930a042e26ac7cd54f41b95f94c0ac91e39
Seonggyu-Min <minsunggu705@gmail.com>
2025-05-30 17:32:20 +0900

    feat: 몬스터 겹침 방지 기능 추가
    - 자식 오브젝트에 Feet을 추가하여 Rigidbody와 Collider를 사용한 결과 Feet 오브젝트가 부모오브젝트와 동떨어지는 현상이 발생함
    - 따라서 OverlapCircleAll 메서드를 활용하여 범위 안에 몬스터 감지 시 본체가 밀려나는 기능 추가

---
+++ Assets/Scripts/Monsters/MonsterFeetPusher.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MonsterFeetPusher : MonoBehaviour
+{
+    [SerializeField] private float _feetCheckRadius = 0.3f;
+    [SerializeField] private LayerMask _monsterLayer = 1 << 15;
+    [SerializeField] private Transform _feetTransform;
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private float _pushForce = 5f;
+
+    private bool _isDead = false;
+
+    private void FixedUpdate()
+    {
+        if (!_isDead)
+        {
+            PushByMonster();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_feetTransform.position, _feetCheckRadius);
+    }
+
+    private void PushByMonster()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(_feetTransform.position, _feetCheckRadius, _monsterLayer);
+
+        foreach (var other in colliders)
+        {
+            if (other.transform == _feetTransform)
+            {
+                continue;
+            }
+
+            Vector2 dir = (_feetTransform.position - other.transform.position).normalized;
+            _rb.AddForce(dir * _pushForce);
+        }
+    }
+
+    public void SetDead(bool isDead)
+    {
+        _isDead = isDead;
+    }
+}

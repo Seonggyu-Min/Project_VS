984df2f014cfdcbd9d478c8ebff9f1dfdb0f8d27
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-03 02:08:20 +0900

    feat: PickUp 아이템 공통 로직 추상 클래스로 구현
    - EXPGem의 로직 해당 클래스로 이전
    - 사운드 재생, ReturnPool, 이동 등 공동 로직 virtual로 구현

--- Assets/Scripts/PowerUps/BasePickUpsBehaviour.cs
+++ Assets/Scripts/PowerUps/BasePickUpsBehaviour.cs
@@ -2,7 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasePickUpsBehaviour : MonoBehaviour
+public abstract class BasePickUpsBehaviour<T> : PooledObject<T> where T : BasePickUpsBehaviour<T>
 {
+    [SerializeField] protected LayerMask _playerLayer = 1 << 7;
+    [SerializeField] protected float _magnetRange; // TODO: 플레이어에게 받아와야 됨
+    [SerializeField] protected float _moveSpeed = 5f;
 
+    [SerializeField] protected AudioSource _expSoundSource;
+
+    protected PlayerStatManager _playerStatManager;
+    protected Transform _playerTransform;
+    protected Coroutine _returnCoroutine;
+
+    protected virtual void OnEnable()
+    {
+        transform.localScale = Vector3.one; // 원래 크기로 복구
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        MoveToPlayer();
+    }
+
+    protected virtual void MoveToPlayer()
+    {
+        if (_playerStatManager == null)
+        {
+            _playerStatManager = PlayerStatManager.Instance;
+            _playerTransform = _playerStatManager.transform;
+        }
+
+        _magnetRange = _playerStatManager.MagnetRangeMultiplier;
+
+        if (CalculateDist(_playerTransform) <= _magnetRange)
+        {
+            transform.Translate(((_playerTransform.position - transform.position).normalized) * Time.fixedDeltaTime * _moveSpeed);
+        }
+    }
+
+    protected virtual float CalculateDist(Transform playerTransform)
+    {
+        float dist = Vector2.Distance(transform.position, playerTransform.position);
+        return dist;
+    }
+
+    protected virtual void PlayPickUpSound()
+    {
+        float randomPitch = Random.Range(0.8f, 1.2f);
+        _expSoundSource.pitch = randomPitch;
+        _expSoundSource.volume = GameManager.Instance.AudioManager.SFXVolume;
+        _expSoundSource.Play();
+    }
+
+    protected virtual IEnumerator ReturnRoutine()
+    {
+        yield return new WaitForSeconds(1f);
+        ReturnPool();
+    }
 }

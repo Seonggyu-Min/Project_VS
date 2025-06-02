2b73c22a37867a38722b1e842368a952699fddc8
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-03 01:12:17 +0900

    feat: 싱글톤 매니저 변경
    - 기존의 매니저 삭제 및 AudioManager, InGameCountManager를 싱글톤으로 변경

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,25 +1,20 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    public SpawnManager SpawnManager { get; private set; }
-    public SkillManager SkillManager { get; private set; }
-    public SkillPoolManager SkillPoolManager { get; private set; }
-
+    public InGameCountManager InGameCountManager { get; private set; }
+    public AudioManager AudioManager { get; private set; }
 
     private void Awake()
     {
         Init();
     }
-
-
     private void Init()
     {
         SingletonInit();
-        SpawnManager = GetComponentInChildren<SpawnManager>();
-        SkillManager = GetComponentInChildren<SkillManager>();
-        SkillPoolManager = GetComponentInChildren<SkillPoolManager>();
+        InGameCountManager = GetComponentInChildren<InGameCountManager>();
+        AudioManager = GetComponentInChildren<AudioManager>();
     }
 }

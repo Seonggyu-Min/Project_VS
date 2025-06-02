ade82c8ff548adc0738975888749350ab3308897
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-03 01:02:59 +0900

    refactor: 가독성 리팩터링

--- Assets/Scripts/Skills/FlameGround.cs
+++ Assets/Scripts/Skills/FlameGround.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -17,52 +17,12 @@ public class FlameGround : BaseSkill
 
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
-        if (((1 << collision.gameObject.layer) & _monsterLayermask) != 0)
-        {
-            ReferenceProvider provider = ReferenceRegistry.GetProvider(collision.gameObject);
-
-            if (provider == null)
-            {
-                Debug.LogWarning($"ReferenceProvider가 {collision.gameObject.name}에 없음");
-                return;
-            }
-
-            IDamageable damageable = provider.GetAs<IDamageable>();
-
-            if (damageable != null)
-            {
-                _targetsList.Add(damageable);
-            }
-            else
-            {
-                Debug.LogWarning($"IDamageable이 {collision.gameObject.name}에 없음");
-            }
-        }
+        HandleTriggerEnter(collision);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (((1 << collision.gameObject.layer) & _monsterLayermask) != 0)
-        {
-            ReferenceProvider provider = ReferenceRegistry.GetProvider(collision.gameObject);
-
-            if (provider == null)
-            {
-                Debug.LogWarning($"ReferenceProvider가 {collision.gameObject.name}에 없음");
-                return;
-            }
-
-            IDamageable damageable = provider.GetAs<IDamageable>();
-
-            if (damageable != null)
-            {
-                _targetsList.Remove(damageable);
-            }
-            else
-            {
-                Debug.LogWarning($"IDamageable이 {collision.gameObject.name}에 없음");
-            }
-        }
+        HandleTriggerExit(collision);
     }
 
     public override void InitSkillPosition()
@@ -105,6 +65,56 @@ public class FlameGround : BaseSkill
             }
 
             _targetsList[i].TakeDamage(_damage);
+        }
+    }
+
+    private void HandleTriggerEnter(Collider2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & _monsterLayermask) != 0)
+        {
+            ReferenceProvider provider = ReferenceRegistry.GetProvider(collision.gameObject);
+
+            if (provider == null)
+            {
+                Debug.LogWarning($"ReferenceProvider가 {collision.gameObject.name}에 없음");
+                return;
+            }
+
+            IDamageable damageable = provider.GetAs<IDamageable>();
+
+            if (damageable != null)
+            {
+                _targetsList.Add(damageable);
+            }
+            else
+            {
+                Debug.LogWarning($"IDamageable이 {collision.gameObject.name}에 없음");
+            }
+        }
+    }
+
+    private void HandleTriggerExit(Collider2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & _monsterLayermask) != 0)
+        {
+            ReferenceProvider provider = ReferenceRegistry.GetProvider(collision.gameObject);
+
+            if (provider == null)
+            {
+                Debug.LogWarning($"ReferenceProvider가 {collision.gameObject.name}에 없음");
+                return;
+            }
+
+            IDamageable damageable = provider.GetAs<IDamageable>();
+
+            if (damageable != null)
+            {
+                _targetsList.Remove(damageable);
+            }
+            else
+            {
+                Debug.LogWarning($"IDamageable이 {collision.gameObject.name}에 없음");
+            }
         }
     }
 }

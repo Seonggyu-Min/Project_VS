cd9d14e6611322af5904daecdfabb945cf1c8f5e
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-04 16:32:59 +0900

    feat: Magnet 아이템 기능 추가
    - 획득 시 현존하는 모든 경험치 보석을 끌어들이는 아이템 추가
    - PickUpsManager에서 경험치 보석을 미리 캐싱하여 MagnetAllEXPGems 메서드 호출로 일괄 처리

--- Assets/Scripts/PowerUps/MagnetPowerUpBehaviour.cs
+++ Assets/Scripts/PowerUps/MagnetPowerUpBehaviour.cs
@@ -2,8 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MagnetPowerUpBehaviour : MonoBehaviour
+public class MagnetPowerUpBehaviour : BasePickUpsBehaviour<MagnetPowerUpBehaviour>
 {
-    
+    [SerializeField] private Animator _animator;
 
+    private bool _isMagnetActive = false;
+    private readonly int _magnetHash = Animator.StringToHash("Magnet");
+
+    protected override void OnEnable()
+    {
+        _isMagnetActive = false;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & _playerLayer) != 0)
+        {
+            if (_isMagnetActive)
+            {
+                return;
+            }
+
+            _isMagnetActive = true;
+
+            PlayPickUpSound();
+            _animator.Play(_magnetHash);
+
+            PickUpsManager.Instance.MagnetAllEXPGems();
+        }
+    }
+    
+    // 애니메이션 이벤트용
+    private void DisableAnimation()
+    {
+        ReturnPool();
+    }
 }
